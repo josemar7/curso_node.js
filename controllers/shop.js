4fcc9e657ee54d178e4ecc264913ec7aa88cf193
josemar7 <josemar7@gmail.com>
2018-11-23 07:42:22 +0100

    mi commit!

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,4 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
-
 
 // exports.getProducts = (req, res, next) => {
 //     Product.fetchAll(products => {
@@ -147,32 +145,114 @@ exports.getCart = (req, res, next) => {
     .catch(err => console.log(err));
 };
 
+// exports.postCart = (req, res, next) => {
+//     const prodId = req.body.productId;
+//     Product.findById(prodId, product => {
+//         Cart.addProduct(prodId, product.price);
+//     });    
+//     res.redirect('/cart');
+// };
 exports.postCart = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findById(prodId, product => {
-        Cart.addProduct(prodId, product.price);
-    });    
-    res.redirect('/cart');
+    let fetchedCart;
+    let newQuantity = 1;
+    req.user
+    .getCart()
+    .then(cart => {
+        fetchedCart = cart;
+      return cart.getProducts({where: {id: prodId}});
+    })
+    .then(products => {
+        let product;
+        if (products.length > 0) {
+            product = products[0];
+        }        
+        if (product) {
+            const oldQuantity = product.cartItem.quantity;
+            newQuantity = oldQuantity + 1;
+            return product;            
+        }
+        return Product.findById(prodId)        
+    })
+    .then(product => {
+        return fetchedCart.addProduct(product, {through: {quantity: newQuantity}});
+    })
+    .then(() => {
+        res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
 };
 
+// exports.postCartDeleteProduct = (req, res, next) => {
+//     const prodId = req.body.productId;
+//     Product.findById(prodId, p => {
+//         Cart.deleteProduct(prodId, p.price);
+//         res.redirect('/cart');
+//     });    
+// };
 exports.postCartDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findById(prodId, p => {
-        Cart.deleteProduct(prodId, p.price);
-        res.redirect('/cart');
-    });    
-};
-
-exports.getOrders = (req, res, next) => {
-    res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'Your order'
-    });
-};
-
-exports.getCheckout = (req, res, next) => {
-    res.render('shop/checkout', {
-        path: '/checkout',
-        pageTitle: 'Checkout'
+    req.user.getCart()
+    .then(cart => {
+        return cart.getProducts({where: {id: prodId}});
     })
+    .then(products => {
+        const product = products[0];
+        return product.cartItem.destroy();
+    })
+    .then(result => {
+        res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
 };
+
+exports.postOrder = (req, res, next) => {
+    let fetchedCart;
+    req.user.getCart()
+    .then(cart => {
+        fetchedCart = cart;
+        return cart.getProducts();
+    })
+    .then(products => {
+        return req.user.createOrder()
+        .then(order => {
+            return order.addProducts(products.map(product => {
+                product.orderItem = {quantity: product.cartItem.quantity};
+                return product;
+            }));
+        })
+        .then(result => {
+            return fetchedCart.setProducts(null);            
+        })
+        .then(result => {
+            res.redirect('/orders');
+        })
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+};
+
+// exports.getOrders = (req, res, next) => {
+//     res.render('shop/orders', {
+//         path: '/orders',
+//         pageTitle: 'Your order'
+//     });
+// };
+exports.getOrders = (req, res, next) => {
+    req.user.getOrders({include: ['products']})
+    .then(orders => {
+        res.render('shop/orders', {
+            path: '/orders',
+            pageTitle: 'Your order',
+            orders: orders
+        });
+    })
+    .catch(err => console.log(err));    
+};
+
+// exports.getCheckout = (req, res, next) => {
+//     res.render('shop/checkout', {
+//         path: '/checkout',
+//         pageTitle: 'Checkout'
+//     })
+// };

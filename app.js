a1a5196c14bff83da677a3b2e1e09d903a21faf2
josemar <josemar7@gmail.com>
2018-11-25 17:57:02 +0100

    mi commit!

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 const errorController = require('./controllers/error');
 // const db = require('./util/database');
 // const sequelize = require('./util/database');
@@ -56,6 +57,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
--- controllers/shop.js
+++ controllers/shop.js
@@ -52,7 +52,8 @@ exports.getProducts = (req, res, next) => {
             res.render("shop/product-list", {
                 prods: products,
                 pageTitle: "All products",
-                path: "/products"
+                path: "/products",
+                isAuthenticated: req.isLoggedIn
             });
         })
         .catch(err => console.log(err));
@@ -109,7 +110,8 @@ exports.getProduct = (req, res, next) => {
         res.render('shop/product-detail', {
             product: product,
             pageTitle: product.title,
-            path: '/products'
+            path: '/products',
+            isAuthenticated: req.isLoggedIn
         });
     })
     .catch(err => console.log(err));
@@ -165,7 +167,8 @@ exports.getIndex = (req, res, next) => {
         res.render('shop/index', {
             prods: products,
             pageTitle: 'Shop',
-            path: '/'
+            path: '/',
+            isAuthenticated: req.isLoggedIn
         });
     })
     .catch(err => console.log(err));
@@ -229,7 +232,8 @@ exports.getCart = (req, res, next) => {
             res.render("shop/cart", {
                 path: "/cart",
                 pageTitle: "Your cart",
-                products: products
+                products: products,
+                isAuthenticated: req.isLoggedIn
             });
         })
         .catch(err => console.log(err));
@@ -424,7 +428,8 @@ exports.getOrders = (req, res, next) => {
         res.render('shop/orders', {
             path: '/orders',
             pageTitle: 'Your order',
-            orders: orders
+            orders: orders,
+            isAuthenticated: req.isLoggedIn
         });
     })    
     .catch(err => console.log(err));    

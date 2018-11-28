3f4d2b6848ab93ee29d05a6e849409d8e1c64af7
josemar <josemar7@gmail.com>
2018-11-28 13:14:44 +0100

    mi commit!

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,6 +5,8 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+const ITEMS_PER_PAGE = 1;
+
 // exports.getProducts = (req, res, next) => {
 //     Product.fetchAll(products => {
 //         res.render('shop/product-list', {
@@ -50,14 +52,29 @@ const Order = require('../models/order');
 //       .catch(err => console.log(err));
 //   };
 exports.getProducts = (req, res, next) => {
+    const page = +req.query.page || 1;
+    let totalItems;
     Product.find()
+        .countDocuments()
+        .then(numProducts => {
+            totalItems = numProducts;
+            return Product.find()
+                .skip((page - 1) * ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE);
+        })
         .then(products => {
-            console.log(products);
-            res.render("shop/product-list", {
+            res.render('shop/product-list', {
                 prods: products,
-                pageTitle: "All products",
-                path: "/products"
+                pageTitle: 'Products',
+                path: '/products',
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
             });
+            res.render()
         })
         .catch(err => {
             const error = new Error(err);
@@ -172,13 +189,29 @@ exports.getProduct = (req, res, next) => {
 //     .catch(err => console.log(err));
 // };
 exports.getIndex = (req, res, next) => {
+    const page = +req.query.page || 1;
+    let totalItems;
     Product.find()
+    .countDocuments()
+    .then(numProducts => {
+        totalItems = numProducts;
+        return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
         res.render('shop/index', {
             prods: products,
             pageTitle: 'Shop',
-            path: '/'
+            path: '/',
+            currentPage: page,
+            hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+            hasPreviousPage: page > 1,
+            nextPage: page + 1,
+            previousPage: page - 1,
+            lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
         });
+        res.render()
     })
     .catch(err => {
         const error = new Error(err);
